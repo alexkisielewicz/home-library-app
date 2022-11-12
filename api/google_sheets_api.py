0f4d0438587375bfda6ebdbf6eca1016c6555e5e
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-11-12 21:52:25 +0000

    Connect Google Sheets API, add requirements.txt

---
+++ api/google_sheets_api.py
@@ -0,0 +1,20 @@
+"""
+Google Sheets API connection and constants
+"""
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('home-library')
+
+# Two worksheets that are used by the app
+LIBRARY = SHEET.worksheet('library')  # database with all books
+CONFIG = SHEET.worksheet('config')  # config to read/write DB sorting method
