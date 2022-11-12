f24dd006e21aaff57814e4d1385b2c6b12726a19
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-11-12 22:16:02 +0000

    Add main function that shows logo and app menu

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+import app_menu as menu
+
+
+def main():
+    """
+    Shows app menu, from where user can start
+    and use all app functionalities.
+    """
+    menu.logo()
+    menu.show_menu()
+
+
+main()
+
+
+# TODO
+# * Validate User input
+# * Exception handling
+# * Terminal colours
+# * Write type hinting
