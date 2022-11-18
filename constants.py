336aee4b8e613e85d77a110170828cc7d11458b0
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-11-18 11:18:26 +0000

    Add constants regarding allowed input length

--- constants.py
+++ constants.py
@@ -5,18 +5,33 @@ Constants used throughout the app
 from api.google_sheets_api import *
 from colorama import Fore, Style
 
-# app constants
+# App constants
 HEADERS = LIBRARY.row_values(1)
 HEADERS_NO_DESC = HEADERS[:-1]
 HEADERS_NO_DESC_NO_ID = HEADERS[:-1]
 DESCRIPTION = LIBRARY.row_values(1).pop()
 ALL_VALUES = LIBRARY.get_all_values()
 ALL_VALUES_NO_HEADER = ALL_VALUES[1:]
-APP = "Home Library"
-LINE = Fore.YELLOW + "###############################################" \
-                     "################################" + Style.RESET_ALL  # 79 characters long
+APP = "Home Library App"
 
-# descriptions of the functionalities
+# PrettyTable columns width
+MAX_LEN = {"ID": 2, "Title": 24, "Author": 18, "Category": 12, "Status": 8}
+# PrettyTable table width
+TABLE_MAX_LEN = 79
+# Book details max length for validation of inputs
+ID_MAX_LEN = MAX_LEN["ID"]
+TITLE_MAX_LEN = MAX_LEN["Title"]
+AUTHOR_MAX_LEN = MAX_LEN["Author"]
+CAT_MAX_LEN = MAX_LEN["Category"]
+# description below assigned individually, it is displayed outside the PrettyTable tab.
+DESC_MAX_LEN = 200
+# Separator line
+LINE = Fore.YELLOW + "#"*TABLE_MAX_LEN + Style.RESET_ALL  # 79 characters long
+# Reading status
+READ_YES = "Read"
+READ_NO = "Not read"
+
+# Descriptions of the functionalities
 ADD_BOOK = Fore.LIGHTYELLOW_EX + """
 Now you can add a new book to your library. \n
 You will be asked to enter book title, author, category and short description.
